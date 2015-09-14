504695692a7c1c33517ae71e08a1b8cfd55a067b
Jeremy <jgertz26@gmail.com>
2015-09-14 17:20:49 -0400

    now it does things

---
+++ spec/features/user_views_questions_index_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+feature 'view all questions', %Q{
+  As a user
+  I want to view all questions
+  So that I can see the them
+} do
+  # Acceptance Criteria
+  # * I must see the body of each question
+  #   Ordered by most recent
+
+  scenario 'user views all questions' do
+    question = Question.create(body: "WUT?")
+
+    visit '/questions'
+    expect(page).to have_content(question.body)
+  end
+
+end
+
+feature 'view all questions', %Q{
+  As a user
+  I want to view a question's details
+  So that I can effectively understand the question
+} do
+
+  # Acceptance Criteria
+  # * - I must be able to get to this page from the questions index
+  # - I must see the question's title
+  # - I must see the question's description
+
+  scenario 'user views all questions' do
+    question = Question.create(body: "WUT?", description: "cuz")
+
+    visit '/questions'
+    click_link question.body
+    expect(page).to have_content(question.body)
+    expect(page).to have_content(question.description)
+  end
+
+end
